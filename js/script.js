a083fbcdf507db77a1d57fd6c55d3a4a9277b0f2
The Claw Club <gavindukavishka.bcg@gmail.com>
2026-03-14 20:31:40 +0530

    Update script.js

--- js/script.js
+++ js/script.js
@@ -9,7 +9,7 @@ const config = {
         serverLogoImageFileName: "logo.png", /*This is a file name for logo in /images/ (If you upload new logo with other name, you must change this value)*/
         serverName: "The Claw Club", /*Server name*/
         serverIp: "The Claw Club", /*Server IP (if you want to add online user counter, you must have true the enable-status and enable-query of server.properties)*/
-        discordServerID: "" /*Your server ID (if you want to add online user counter, you must have enabled Discord server widget)*/
+        discordServerID: "1478551699261227130" /*Your server ID (if you want to add online user counter, you must have enabled Discord server widget)*/
     },
 
     /*Admin-Team
